e814e376acf9a2adaa8f167ec6b67004cb2fee91
commit1 <leonardo.albornos2007@gmail.com>
2026-09-14 08:36:07 -0400

    Proyecto HeroesWeb completo

---
+++ Data/HeroesContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using HeroesWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroesWeb.Data;
+
+public partial class HeroesContext : DbContext
+{
+    public HeroesContext(DbContextOptions<HeroesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Heroes> Heroes { get; set; }
+
+    public virtual DbSet<SuperPoderes> SuperPoderes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Heroes>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Heroes__3214EC0705FF0874");
+        });
+
+        modelBuilder.Entity<SuperPoderes>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__SuperPod__3214EC079350E7BE");
+
+            entity.HasOne(d => d.Heroe).WithMany(p => p.SuperPoderes).HasConstraintName("FK_SuperPoderes_Heroes");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
